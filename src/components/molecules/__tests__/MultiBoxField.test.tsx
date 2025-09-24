7522d2b0ed140e18c862ff1114ed10e481821986
marjo1994 <m_rhl@live.com>
2025-09-24 18:17:08 -0500

    fix: remove unused code

--- src/components/molecules/__tests__/MultiBoxField.test.tsx
+++ src/components/molecules/__tests__/MultiBoxField.test.tsx
@@ -1,6 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 import { FormProvider, useForm } from "react-hook-form";
 import { MultiListBoxField } from "../MultiListBoxField";
 import { TaskTag } from "../../../__generated__/graphql";
